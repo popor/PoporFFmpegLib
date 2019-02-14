db47596fb2a01c38280d7513d51850483bf30def
wangkq <908891024@qq.com>
2019-02-14 16:18:16 +0800

    init

---
+++ PoporFFmpeg/PoporFFmpeg.h
@@ -0,0 +1,25 @@
+//
+//  PoporFFmpeg.h
+//  PoporFFmpeg
+//
+//  Created by apple on 2019/2/14.
+//  Copyright © 2019 popor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+typedef void(^PoporFFmpegFinishBlock)(BOOL finished, NSString * info); // __BlockTypedef
+
+@interface PoporFFmpeg : NSObject
+
+@property (nonatomic, copy  ) PoporFFmpegFinishBlock finishBlock;
++ (CGSize)sizeFrom:(CGSize)originSize targetSize:(CGSize)targetSize;
++ (CGSize)videoSizeFromUrl:(NSString *)url;
+
+- (void)compressCmd:(NSString *)cmd finish:(PoporFFmpegFinishBlock)finishBlock;
+
+- (void)compressVideoUrl:(NSString *)url size:(CGSize)tSize tPath:(NSString *)tPath finish:(PoporFFmpegFinishBlock)finishBlock;
+
+
+@end
